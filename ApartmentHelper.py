62fb497d18c26e3337c3e6fec4d6f97d6b7962c8
Admiralhunter <hunterpalcich18@gmail.com>
2019-06-07 00:39:54 -0400

    Finished weather data section and created beginning of Areavibe's Demographic data.

--- ApartmentHelper.py
+++ ApartmentHelper.py
@@ -1,27 +1,32 @@
+import json
 import time
+from pprint import pprint
 
 from selenium import webdriver
 
 #  Opens Chrome to start the process of gathering data
-inputs = {}
-inputs['location'] = "Grove City, Ohio"
-inputs['Zip Code'] = 43123
+inputs = dict()
 
+#  City format: (city, ST)
+inputs['location'] = "Grove City, OH"
+inputs['Zip Code'] = 43123
 
 
 def main():
 
     #  Opens up chrome and goes to weatherspark to gather historical weather data
+
     driver = webdriver.Chrome("D:\chromedriver_win32/chromedriver.exe")
     driver.get("https://weatherspark.com/")
     driver.maximize_window()
+
     assert "The Typical Weather Anywhere on Earth - Weather Spark" in driver.title
     #  enters the location of the city
     elem = driver.find_element_by_xpath("//input[@title = 'Enter City or Airport']")
     elem.clear()
     elem.send_keys(inputs['location'])
     time.sleep(1)
-    #clicks the value of the first location (hopefully the correct location)
+    #  clicks the value of the first location (hopefully the correct location)
     elem = driver.find_element_by_xpath("//div[@class = 'LiveSearch-options LiveSearch-FrontPage-options']/div[@data-index = '0']")
     elem.click()
     time.sleep(1)
@@ -29,25 +34,34 @@ def main():
     #  dictionary to hold all values of the weather for location
     weather = {}
 
-    get_value_xpath(driver, weather, 'Location Description', "//div[@class = 'Section-body charts_only_do_hide'][1]//p[1]")
-    get_value_xpath(driver, weather, 'Best Tourism Time', "//div[@class = 'Section-body charts_only_do_hide'][1]//p[2]")
+    with open('Weather.json') as f:
+        weatherData = json.load(f)
 
-    get_value_xpath(driver, weather, 'Warm Season', "//div[@class = 'Section-body charts_only_do_hide'][2]/p[1]")
-    get_value_xpath(driver, weather, 'Cold Season', "//div[@class = 'Section-body charts_only_do_hide'][2]/p[2]")
+    for val in weatherData['weather']:
+        get_value_xpath(driver, weather, val, weatherData['weather'][val])
 
-    get_value_xpath(driver, weather, 'Wet Season', "//div[@class = 'Section-body charts_only_do_hide'][4]/p[2]")
-    get_value_xpath(driver, weather, 'Dry Season', "//div[@class = 'Section-body charts_only_do_hide'][4]/p[3]")
-    get_value_xpath(driver, weather, 'Snowfall', "//div[@class = 'Section-body charts_only_do_hide'][4]/p[10]")
-    get_value_xpath(driver, weather, 'Humidity', "//div[@class = 'Section-body charts_only_do_hide'][6]/p[3]")
-    get_value_xpath(driver, weather, 'Surface Layout', "//div[@class = 'Section-body charts_only_do_hide'][11]/p[3]")
-
-
-
-
-    print(weather)
     for val in weather:
         print(val + ': ' + weather[val])
 
+    #  This section will go to Areavibe and gather demographics for the city
+    cityUrl = inputs['location'].split(", ")[0]
+    cityUrl = cityUrl.replace(' ', '+')
+    cityUrl = cityUrl.lower()
+    driver.get("https://www.areavibes.com/ax_search_full/?query=" + cityUrl)
+
+    city = inputs['location'].split(", ")[0]
+    state = inputs['location'].split(", ")[1]
+    areaVibeLocation = json.loads(driver.find_element_by_tag_name("body").text)
+    pprint(areaVibeLocation)
+    for val in areaVibeLocation:
+        stateCheck = val['state_abbr']
+        cityCheck = val['city']
+        if stateCheck == state and cityCheck == city:
+            lat = val['lat']
+            lon = val['lon']
+            break
+
+    driver.get("https://www.areavibes.com/" + cityUrl +"-" + state.lower() + "/livability/?ll=" + str(lat) + "+" + str(lon))
     driver.quit()
 
 
@@ -55,6 +69,7 @@ def main():
 def get_value_xpath(driver, dictionary, key, xpath):
     elem = driver.find_element_by_xpath(xpath).text
     dictionary[key] = elem
+
 
 
 if __name__ == "__main__":
